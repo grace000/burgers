3795077e614e4ea84b84d23c961cd2550929d7c2
Tiffani <tiffani.grace.elmore@gmail.com>
2017-10-03 23:53:47 -0500

    added heroku postgres

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,18 @@
+
+
 // Set up MySQL connection.
 var mysql = require("mysql");
+
+  if (process.env.HEROKU_POSTGRESQL_GREEN_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
 
 var connection = mysql.createConnection({
   port: 3306,
@@ -8,6 +21,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "burger"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
